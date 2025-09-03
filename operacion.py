cd3fe8cbdb36ae90ce77a9794acf1585520c07e6
mariana01-ca <mariana.condori.a@gmail.com>
2025-09-03 11:43:18 -0400

    mi operacion python

---
+++ operacion.py
@@ -0,0 +1,8 @@
+numero1 =5
+numero2 =10
+suma= numero1+numero2
+print('la suma del numero1: ', numero1,'y ',numero2,'es: ',suma)
+print('MARIANA NOEMI CONDORI APAZA')
+print('JOSE ARMANDO MAMANI MADRIAGA')
+print('JHOJAN ALDAIR SANCHEZ LUIS')
+print('ANA GARCIA ARUQUIPA VARGAS')

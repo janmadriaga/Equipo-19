3be895dfb0f885c5b376ef2dc80993b1a3d1c978
Anaavar777 <aaruquipav@fcpn.edu.bo>
2025-09-03 11:40:41 -0400

    mi archivo saludo java

---
+++ Saludo.java
@@ -0,0 +1,10 @@
+public class Saludo {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola equipo!");
+        System.out.println("Ana Gloria Aruquipa Vargas");
+        System.out.println("Jose Armando Mamani Madriaga");
+        System.out.println("Jhojan Aldair Sanchez Luis");
+        System.out.println("Mariana Noemi Condori Apaza");
+    }
+}
